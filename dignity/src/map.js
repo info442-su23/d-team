5aee6dd52a10b1365ed107999ea9c8e72149255d
Calvin Chan <calvinchan@Calvins-MacBook-Pro.local>
2023-07-23 18:05:37 +0800

    Added marker to the map

--- dignity/src/map.js
+++ dignity/src/map.js
@@ -1,48 +1,49 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '400px',
-  height: '400px'
+  height: '400px',
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
 
 function Map() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyCptULJKSbbS6Oad0nFWiHEImiMkPrpDC0"
-  })
+    googleMapsApiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = React.useCallback(function callback(mapInstance) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    mapInstance.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+    // Store the map instance in the state variable
+    setMap(mapInstance);
+  }, []);
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+  const onUnmount = React.useCallback(function callback() {
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Add a marker to the map */}
+      {map && <Marker position={center} map={map} />}
+    </GoogleMap>
+  ) : <></>;
 }
 
-export default React.memo(Map)
+export default React.memo(Map);
