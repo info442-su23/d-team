63c9d289534bcb0d264c12a39c1743709e9b97c2
Calvin Chan <calvinchan@Calvins-MacBook-Pro.local>
2023-07-23 18:37:51 +0800

    Added back Google Maps API key

--- dignity/src/map.js
+++ dignity/src/map.js
@@ -14,7 +14,7 @@ const center = {
 function Map() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+    googleMapsApiKey: "AIzaSyCptULJKSbbS6Oad0nFWiHEImiMkPrpDC0",
   });
 
   const [map, setMap] = React.useState(null);
