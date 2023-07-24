aaba05fc23ec562f8f04f6a06ddede53d33c9fa4
syd-moral <smoral@uw.edu>
2023-07-24 13:19:32 -0700

    render creator page+ created components folder

--- dignity/src/components/App.js
+++ dignity/src/components/App.js
@@ -1,10 +1,12 @@
-import "./App.css";
-import Map from "./map"
+import "../App.css";
+import Map from "./map";
+import Creator from "./Creator";
 
 const App = () => {
   return (
     <div className="App">
       <Map />
+      <Creator />
     </div>
   );
 };
