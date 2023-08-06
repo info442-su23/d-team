8ab855dbf254b113ae657d46f6f9e88b1d2eec98
elisabeth-helleberg <ehelle@uw.edu>
2023-08-06 01:19:28 -0700

    add figma code for home page, map page, and profile page

---
+++ dignity/src/components/HomePage.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export default function HomePage() {
+
+    return (
+        <div className="home">
+            <div className="overlap-group-wrapper">
+                <div className="overlap-group">
+                    <img className="image" alt="Image" src="image-1.png" />
+                    <h1 className="element-people-in">
+                        <span className="text-wrapper">53,500</span>
+                        <span className="span"> people in King County experienced homelessness in 2022...</span>
+                    </h1>
+                    <p className="washington-s">Washington’s Department Of Commerce, 2023</p>
+                    <div className="div" />
+                    <div className="rectangle-2" />
+                    <p className="dignity-empowering">
+                        <span className="text-wrapper-2">Dignity: Empowering Change in Seattle&#39;s Homelessness Crisis</span>
+                        <span className="text-wrapper-3">
+                            . With a staggering surge in the homeless population to 53,500 in 2022, up from 45,300 in 2019, our
+                            mission is urgent. As{" "}
+                        </span>
+                        <span className="text-wrapper-2">
+                            Washington has become the fourth highest state in the U.S with the largest homeless population
+                        </span>
+                        <span className="text-wrapper-3">
+                            , Seattle demands our attention. To combat this growing issue, Dignity offers a platform that makes
+                            volunteering easier and accessible to everyone. Join us in making a difference, as we work together to
+                            address Seattle&#39;s homelessness problem and restore dignity to those in need.
+                        </span>
+                    </p>
+                    <div className="title">
+                        <img className="line" alt="Line" src="image.svg" />
+                        <img className="img" alt="Line" src="line-2-2.svg" />
+                    </div>
+                    <div className="text-wrapper-4">Dignity’s Goal</div>
+                    <div className="text-wrapper-5">Volunteer</div>
+                    <div className="title-2">
+                        <img className="line-2" alt="Line" src="line-1-2.svg" />
+                        <img className="line-3" alt="Line" src="line-2-3.svg" />
+                    </div>
+                    <p className="check-out-volunteer">Check Out Volunteer Opportunities In Your Area On The Opportunity Map!</p>
+                    <Rectangle className="rectangle-4" />
+                    <div className="text-wrapper-6">Explore Map!</div>
+                    <div className="rectangle-3" />
+                    <div className="text-wrapper-7">About Us</div>
+                    <div className="title-3">
+                        <img className="line" alt="Line" src="line-1.svg" />
+                        <img className="line-4" alt="Line" src="line-2.svg" />
+                    </div>
+                    <div className="rectangle-5" />
+                    <div className="rectangle-6" />
+                    <div className="rectangle-7" />
+                    <div className="rectangle-8" />
+                    <img className="crop-square" alt="Crop square" src="dsc01460-crop-square-1.png" />
+                    <img className="element" alt="Element" src="65dc477c-4534-419a-8b17-a56a606fc422-1.png" />
+                    <div className="sydney-morales">&nbsp;&nbsp;Sydney Morales</div>
+                    <div className="text-wrapper-8">Elisabeth Helleberg</div>
+                    <div className="text-wrapper-9">Steven Hu</div>
+                    <div className="project-manager"> Project Manager</div>
+                    <img className="ddff-d" alt="Ddff d" src="dd2175ff-48d4-4180-bf2b-67c27b1ae36e-1.png" />
+                    <img className="element-fabb-a" alt="Element" src="9fab2b27-6919-449a-b7c4-2ee74a51af13-1.png" />
+                    <div className="text-wrapper-10">Product Designer</div>
+                    <div className="text-wrapper-11">Developer</div>
+                    <div className="text-wrapper-12">Calvin Chan</div>
+                    <div className="text-wrapper-13">Developer</div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
