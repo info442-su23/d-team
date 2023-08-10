129720a95b8f333eadfbb9b87e7484048a3e9225
syd-moral <97127428+syd-moral@users.noreply.github.com>
2023-08-10 08:16:39 -0700

    Revert "css changes"

--- dignity/src/components/Footer.js
+++ dignity/src/components/Footer.js
@@ -5,10 +5,9 @@ export default function Footer() {
     return (
         <div className="footer-UI">
             <div className="DIGNITY">Dignity</div>
-            <div className="contact">
-                Contact Us:
-                info@dignity.com | 
-                (206)-123-4567
+            <div className="contact-us">
+                <div className="email">info@dignity.com</div>
+                <div className="phone">(206)-123-4567</div>
             </div>
             <div className="divider" />
             <div className="element-tag">© 2023 Dignity | All Rights Reserved</div>
